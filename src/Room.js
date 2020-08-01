f3095a43961d9ee700658f9c440cb52483e86a0c
khurrams84 <khurram.net@gmail.com>
2020-08-01 03:15:32 -0500

    update react app

---
+++ src/Room.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Room() {
+    const [isLit, setLit] = useState(false);
+    let [age, setAge] = useState(24);
+
+    function updateLight() {
+        console.log("light buton clicked!");
+        setLit(!isLit);        
+    }
+
+    const updateAge = () => {
+        console.log("age buton clicked!");       
+        setAge(++age);
+    }
+
+    return (
+    <div>
+        This Room is  {isLit ? "lit" : "dark"}
+        <br/>
+        Age: {age}
+        <br/>
+        <button onClick={updateLight} >Toggle Light</button>
+        <br/>
+        <button onClick={updateAge} >Update Age</button>
+    </div>
+  );
+}
+
+export default Room;
